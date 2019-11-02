4cb8044fab143e869d9ff8aa27e793462c2bd100
thomasmodeneis <thomas.modeneis@gmail.com>
2019-11-02 13:07:31 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,12 +10,12 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
-echo 'www.ethereum.org' > CNAME
+echo 'www.smilo.foundation' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:ethereum/ethereum-org-website.git master:gh-pages
+git push -f git@github.com:smilofoundation/smilo.foundation-website.git master:gh-pages
 
 cd -
